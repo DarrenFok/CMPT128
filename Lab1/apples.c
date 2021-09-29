768773fa6c980e25d723c117fad0f072c357018b
darrenfok <66759042+darrenfok@users.noreply.github.com>
2021-09-29 11:48:08 -0700

    Add files via upload
    added rejection of negative value inputs

--- Lab1/apples.c
+++ Lab1/apples.c
@@ -8,7 +8,6 @@
  */
 
 #include <stdio.h> 
-#include <math.h>
 
 int main (void) {
     //Fields
@@ -19,24 +18,31 @@ int main (void) {
     int result = 0; 
 
     //Prompt user to input amount of apples then amount in each bag
-    printf("Please, enter the number of apples available and the numbers of apples each bag holds: ");
+    printf("Please, enter the number of apples available and the number of apples each bag holds: ");
 
     //Allow for user input and loop process as long as user input is not ctrl+d
     while ((result = scanf("%d %d", &numApples, &inEachBag)) != EOF){
-        //Print number of apples and amount in each bag to user
-        printf("Number of apples available => %d, number of apples each bag holds => %d.\n", numApples, inEachBag);
 
-        //Calculate amount of bags
-        bagsProduced = floor(numApples / inEachBag);
+        //Check whether input is negative, if it is, tell user input is invalid, otherwise, go through.
+        if (numApples <= 0 || inEachBag <= 0){
+            printf("Inputs %d and %d are invalid. Please try again with input values above zero.", numApples, inEachBag);
+        }
 
-        //Calculate amount of leftovers
-        leftOver = floor(numApples % inEachBag);
+        else{
+            //Print number of apples and amount in each bag to user
+            printf("Number of apples available => %d, number of apples each bag holds => %d.\n", numApples, inEachBag);
 
-        //Print results
-        printf("Number of bags produced => %d, number of apples left over => %d.\n", bagsProduced, leftOver);
+            //Calculate amount of bags
+            bagsProduced = numApples / inEachBag;
 
-        //Prompt user to input amount of apples then amount in each bag (included in loop)
-        printf("Please, enter the number of apples available and the numbers of apples each bag holds: ");
+            //Calculate amount of leftovers
+            leftOver = numApples % inEachBag;
+
+            //Print results
+            printf("Number of bags produced => %d, number of apples left over => %d.", bagsProduced, leftOver);
+        }
+        printf("\nPlease, enter the number of apples available and the number of apples each bag holds: ");
+    
     }
 
     //Print "Done."
