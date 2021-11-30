9f9a6eff3c2e1cb070163616eb19d369386ab83e
darrenfok <66759042+darrenfok@users.noreply.github.com>
2021-11-29 16:57:17 -0800

    Add files via upload

--- Lab10/point_array_2.c
+++ Lab10/point_array_2.c
@@ -59,3 +59,23 @@ int point_array_append( point_array_t* pa, point_t* p ){
     return 0;
 
 }
+
+// Remove the point at index i from the array, reducing the number of elements
+// stored in the array by one. The order of points in the array may change.
+// If successful, return 0, else return 1. 
+int point_array_remove( point_array_t* pa, unsigned int i ){
+    if(i >= pa->size || pa->size = 0){
+        return 1;
+    }
+    //switch desired element with last element, and get rid of last element
+    pa->points[i].x = pa->points[pa->size-1].x;
+    pa->points[i].y = pa->points[pa->size-1].y;
+    pa->points[i].z = pa->points[pa->size-1].z;
+    //set last element's fields to empty and decrease size
+    pa->points[pa->size-1].x = 0;
+    pa->points[pa->size-1].y = 0; 
+    pa->points[pa->size-1].z = 0;
+
+    pa->size = pa->size-1;
+    return 0; 
+}
